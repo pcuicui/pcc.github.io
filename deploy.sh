407f319fd0e798a74b46028c6aa19a038f3d6591
xiacao <xiacao@wacai.com>
2021-09-24 11:33:18 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:pcuicui/pcuicui.github.io.git master
+git push -f git@github.com:pcuicui/pcuicui.github.io.git master:gh-pages
 
 cd -
